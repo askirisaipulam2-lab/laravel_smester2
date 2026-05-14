c864145d88de5e659e47ac68a2189eaeb6b60b84
askiri <askirisaipulam2@gmail.com>
2026-05-14 20:45:14 +0700

    pertemuan10

--- app/Models/Staff.php
+++ app/Models/Staff.php
@@ -1,8 +1,17 @@
 <?php
+
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
+
 class Staff extends Model
 {
-    protected $table = 'staff';
-    protected $fillable = ['nip', 'nama', 'gender', 'alamat', 'email', 'foto'];
+    protected $fillable = [
+        'nip',
+        'name',
+        'gender',
+        'alamat',
+        'email',
+        'foto'
+    ];
 }
